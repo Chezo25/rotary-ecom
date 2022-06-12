09e3d8715e2f394b7363a4acde0791f2bde7aa4b
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-06-12 19:56:28 -0400

    -add header and footer

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import Link from "next/link";
 
-export const HeroBanner = () => {
+import { urlFor } from "../lib/client";
+
+export const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMMALL TEXT</p>
-        <H3>MidTxt</H3>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">Click Here</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>Real Txt</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,14 @@ const Home = ({ products, bannerData }) => (
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
-    </div>
-    //{" "}
+    </div>{" "}
     <div className="products-container">
-      //{" "}
+      {" "}
       {products?.map((product) => (
         <Product key={product._id} product={product} />
       ))}
     </div>
-    // <FooterBanner footerBanner={bannerData && bannerData[0]} />
-    //{" "}
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />{" "}
   </div>
 );
 
