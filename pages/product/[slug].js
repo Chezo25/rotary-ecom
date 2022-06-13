29bd0636a1863c030b33eb6d81a2cd1635092a63
IslandBoyDev25 <chezling.stoddard@gmail.com>
2022-06-13 08:11:20 -0400

    product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,40 @@
 import React from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
+
+import { client, urlFor } from "../../lib/client";
 
 const ProductDetails = () => {
-  return <div>[slug]</div>;
+  return (
+    <div>
+      <div className="product-details-container">
+        <div>
+          <div className="image-container">
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default ProductDetails;
